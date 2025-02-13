b772cf1dcdd239a4b06918aecfe68d512c922507
noblock <urent111@yandex.ru>
2025-02-13 15:30:39 +0500

    two

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <header className='flex justify-between items-center' >
+        <button>
+          
+        </button>
+
+      </header>
+      <main></main>
+    </div>
   )
 }
 
